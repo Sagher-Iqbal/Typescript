929333226d777c66d90b086c1ec9f1808361bfd3
Sagher-Iqbal <139800587+Sagher-Iqbal@users.noreply.github.com>
2023-07-17 10:37:19 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+//Ist Assignment
+console.log("hello EveryOne!");
+//2nd Assignment
+let fullName = "Sagher Iqbal";
+console.log("Hello " + fullName + "  " + "would you like to learn some typescript today?");
+//3rd Assigment
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+console.log(fullName.toString());
+//4th Assignment
+console.log("Albert Einstein One said \"A person who never made a mistake never tried anything new.\"");
+//5th Assignment
+let author = "Albert Einstein";
+let quote = "\"A person who never made a mistake never tried anything new.\"";
+console.log(author + " " + "One said " + " " + quote);
+//6th Assignment
+let firstName = "Sagher";
+console.log("\t" + firstName);
+console.log(firstName);
+//7th Assignment
+console.log(((((4 * 4) / 2) + 2) - 2));
+//8th Assignment
+console.log(5 + 3);
+console.log(4 * 2);
+console.log(10 - 2);
+console.log(16 / 2);
+//9th Assignment
+let Num = 4;
+console.log("You Favourite Number Is " + Num);
